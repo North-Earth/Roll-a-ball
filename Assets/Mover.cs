28b9c2f880b59e6dcd3eefa0974c20ca4c615a31
Revolt27 <kuckushckin.alex@yandex.ru>
2019-09-10 21:22:25 +0300

    Added script movement of objects

---
+++ Assets/Mover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    #region Filds
+
+    public Vector3 targetPosition;
+    public float speed;
+    public bool isRepeat;
+
+    private Vector3 startingPosition;
+    private bool isFlip;
+
+    #endregion
+
+    #region Methods
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 currentPositin = transform.position;
+        float step = speed * Time.deltaTime;
+
+        if (isRepeat && (currentPositin == startingPosition || currentPositin == targetPosition))
+        {
+            isFlip = !isFlip;
+        }
+
+        if (!isFlip)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startingPosition, step);
+        }
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.001f)
+        {
+            targetPosition *= -1.0f;
+        }
+    }
+
+    #endregion
+}
