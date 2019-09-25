023543fc41e374c316b1ddd4dce9a28afd1007bb
Revolt27 <kuckushckin.alex@yandex.ru>
2019-09-25 21:19:16 +0300

    Added WillAppear script. An object will appear after the touch of the player.

---
+++ Assets/Scripts/WillAppear.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WillAppear : MonoBehaviour
+{
+    #region Fields
+
+    public new GameObject gameObject;
+
+    #endregion
+
+
+    #region Methods
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    #endregion
+}
