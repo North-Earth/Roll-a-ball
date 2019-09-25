e49933fd452646e332254086d7ce4f59c3b3b154
Revolt27 <kuckushckin.alex@yandex.ru>
2019-09-25 21:15:14 +0300

    Added teleportation script to destination

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    #region Fields
+
+    public Vector3 destination;
+
+    #endregion
+
+
+    #region Methods
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.transform.position = destination;
+        }
+    }
+
+    #endregion
+}
